045e97ed05443002d87a5dae8af77f5de720fabf
Pedro Almeida <pedroqalmeida@gmail.com>
2020-03-09 11:25:33 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Provider } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux'
+import { createStore } from 'redux'
+import logo from './logo.svg';
+
+const selectCard = id => ({
+  type: 'SELECT_CARD',
+  id
+})
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SELECT_CARD':
+      return {
+        ...state,
+        selectedCard: action.id
+      }
+  }
+}
+
+const store = createStore(reducer);
+
+class App extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      cards: [ 'a' , 'b', 'c', 'd']
+    }
+  }
+
+  // handleSelect(card) {
+  //   this.setState({ selectedCard: card });
+  // }
+  
+  render() {
+
+    // const mapDispatchToProps = dispatch => ({
+    //   selectCard: id => dispatch(selectCard(id))
+    // })
+
+    const mapDispatchToProps = dispatch => dispatch(selectCard('a'));
+
+    const CardsConnected = () => connect(null, mapDispatchToProps)(Cards)
+
+    return(
+      <Provider store={store}>
+        <div>
+          <CardsConnected 
+            // cards = { this.state.cards }
+          />
+        </div>
+      </Provider>
+    )
+  }
+}
+
+class Cards extends React.Component{
+  constructor(){
+    super();
+  }
+
+  handleSelected(card) {
+    console.log(card);
+    selectCard(card);
+  }
+
+  render(){
+    return null;
+    // return this.props.cards.map( (card, i) => 
+    //   <div key={i} onClick={ () => this.handleSelected(card) }>
+    //     { card }
+    //   </div>
+    // );
+  }
 }
 
 export default App;
